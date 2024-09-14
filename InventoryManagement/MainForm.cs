e90afab8308cea95cf0917d01d435829f98e14b4
Felix <felix.banks02@gmail.com>
2024-09-14 16:35:58 -0400

    Added comments to MainForm.cs

--- InventoryManagement/MainForm.cs
+++ InventoryManagement/MainForm.cs
@@ -18,28 +18,28 @@ namespace InventoryManagement
             InitializeComponent();
         }
 
-        // to show subform inside mainform
+        // To show subform inside mainform
         private Form activeForm = null;
-        private void openChildForm(Form childForm)
+        private void openChildForm(Form childForm)  // Method to open another form inside the main form
         {
-            if(activeForm != null)
+            if(activeForm != null)  // If there's another form already loaded
             {
-                activeForm.Close();
+                activeForm.Close(); // Unload the activeForm so that we can add another one
             }
-            activeForm = childForm;
-            childForm.TopLevel = false;
-            childForm.FormBorderStyle = FormBorderStyle.None;
-            childForm.Dock = DockStyle.Fill;
-            panelMain.Controls.Add(childForm);
-            panelMain.Tag = childForm;
-            childForm.BringToFront();
-            childForm.Show();
+            activeForm = childForm; // Set the activeForm to be childForm that's being passed within the method header
+            childForm.TopLevel = false; // Tells the form that it will be inside another form
+            childForm.FormBorderStyle = FormBorderStyle.None;   // Tells the form to not have a border
+            childForm.Dock = DockStyle.Fill;    // Tells the form to fill the empty space
+            panelMain.Controls.Add(childForm);  // Adds the controls from the child form to the main panel on the mainForm
+            panelMain.Tag = childForm;  // Tells the panel that there's a childForm
+            childForm.BringToFront();   // Pulls the childForm to the front of the window
+            childForm.Show();   // Displays the childForm
         }
 
+        // Method indicating what happens when you click the Users button
         private void btnUsers_Click(object sender, EventArgs e)
         {
-            
-            openChildForm(new UserForm());
+            openChildForm(new UserForm()); // Tells the main form to open the UserForm
         }
 
         // Creates a method to write to the console easier
