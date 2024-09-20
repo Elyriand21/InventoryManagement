065dd58f6f9d0cd3781ec9098f67d7f299324508
Felix <felix.banks02@gmail.com>
2024-09-20 15:13:12 -0400

    Created the CategoryModuleForm

---
+++ InventoryManagement/CategoryModuleForm.Designer.cs
@@ -0,0 +1,187 @@
+﻿namespace InventoryManagement
+{
+    partial class CategoryModuleForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnClearCategory = new System.Windows.Forms.Button();
+            this.btnUpdateCategory = new System.Windows.Forms.Button();
+            this.btnSaveCategory = new System.Windows.Forms.Button();
+            this.txtCategoryName = new System.Windows.Forms.TextBox();
+            this.lblCategoryName = new System.Windows.Forms.Label();
+            this.btnExit = new System.Windows.Forms.Button();
+            this.lblCustomerModule = new System.Windows.Forms.Label();
+            this.lblCategoryID = new System.Windows.Forms.Label();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.panel1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // btnClearCategory
+            // 
+            this.btnClearCategory.BackColor = System.Drawing.Color.Maroon;
+            this.btnClearCategory.FlatAppearance.BorderSize = 0;
+            this.btnClearCategory.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnClearCategory.ForeColor = System.Drawing.Color.White;
+            this.btnClearCategory.Location = new System.Drawing.Point(433, 129);
+            this.btnClearCategory.Name = "btnClearCategory";
+            this.btnClearCategory.Size = new System.Drawing.Size(85, 41);
+            this.btnClearCategory.TabIndex = 29;
+            this.btnClearCategory.TabStop = false;
+            this.btnClearCategory.Text = "Clear";
+            this.btnClearCategory.UseVisualStyleBackColor = false;
+            // 
+            // btnUpdateCategory
+            // 
+            this.btnUpdateCategory.BackColor = System.Drawing.Color.Olive;
+            this.btnUpdateCategory.FlatAppearance.BorderSize = 0;
+            this.btnUpdateCategory.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnUpdateCategory.ForeColor = System.Drawing.Color.White;
+            this.btnUpdateCategory.Location = new System.Drawing.Point(316, 129);
+            this.btnUpdateCategory.Name = "btnUpdateCategory";
+            this.btnUpdateCategory.Size = new System.Drawing.Size(85, 41);
+            this.btnUpdateCategory.TabIndex = 30;
+            this.btnUpdateCategory.TabStop = false;
+            this.btnUpdateCategory.Text = "Update";
+            this.btnUpdateCategory.UseVisualStyleBackColor = false;
+            // 
+            // btnSaveCategory
+            // 
+            this.btnSaveCategory.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(64)))), ((int)(((byte)(0)))));
+            this.btnSaveCategory.FlatAppearance.BorderSize = 0;
+            this.btnSaveCategory.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnSaveCategory.ForeColor = System.Drawing.Color.White;
+            this.btnSaveCategory.Location = new System.Drawing.Point(199, 129);
+            this.btnSaveCategory.Name = "btnSaveCategory";
+            this.btnSaveCategory.Size = new System.Drawing.Size(85, 41);
+            this.btnSaveCategory.TabIndex = 31;
+            this.btnSaveCategory.TabStop = false;
+            this.btnSaveCategory.Text = "Save";
+            this.btnSaveCategory.UseVisualStyleBackColor = false;
+            // 
+            // txtCategoryName
+            // 
+            this.txtCategoryName.Location = new System.Drawing.Point(172, 85);
+            this.txtCategoryName.Name = "txtCategoryName";
+            this.txtCategoryName.Size = new System.Drawing.Size(370, 23);
+            this.txtCategoryName.TabIndex = 24;
+            // 
+            // lblCategoryName
+            // 
+            this.lblCategoryName.AutoSize = true;
+            this.lblCategoryName.Location = new System.Drawing.Point(47, 88);
+            this.lblCategoryName.Name = "lblCategoryName";
+            this.lblCategoryName.Size = new System.Drawing.Size(117, 17);
+            this.lblCategoryName.TabIndex = 28;
+            this.lblCategoryName.Text = "Category Name:";
+            // 
+            // btnExit
+            // 
+            this.btnExit.BackColor = System.Drawing.Color.White;
+            this.btnExit.FlatAppearance.BorderSize = 0;
+            this.btnExit.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnExit.Font = new System.Drawing.Font("Century Gothic", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnExit.ForeColor = System.Drawing.Color.Red;
+            this.btnExit.Location = new System.Drawing.Point(551, 11);
+            this.btnExit.Name = "btnExit";
+            this.btnExit.Size = new System.Drawing.Size(44, 36);
+            this.btnExit.TabIndex = 5;
+            this.btnExit.TabStop = false;
+            this.btnExit.Text = "Exit";
+            this.btnExit.UseVisualStyleBackColor = false;
+            // 
+            // lblCustomerModule
+            // 
+            this.lblCustomerModule.AutoSize = true;
+            this.lblCustomerModule.Font = new System.Drawing.Font("Century Gothic", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblCustomerModule.ForeColor = System.Drawing.Color.White;
+            this.lblCustomerModule.Location = new System.Drawing.Point(4, 18);
+            this.lblCustomerModule.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.lblCustomerModule.Name = "lblCustomerModule";
+            this.lblCustomerModule.Size = new System.Drawing.Size(144, 19);
+            this.lblCustomerModule.TabIndex = 0;
+            this.lblCustomerModule.Text = "Category Module";
+            // 
+            // lblCategoryID
+            // 
+            this.lblCategoryID.AutoSize = true;
+            this.lblCategoryID.Location = new System.Drawing.Point(78, 141);
+            this.lblCategoryID.Name = "lblCategoryID";
+            this.lblCategoryID.Size = new System.Drawing.Size(86, 17);
+            this.lblCategoryID.TabIndex = 32;
+            this.lblCategoryID.Text = "Category ID";
+            this.lblCategoryID.Visible = false;
+            // 
+            // panel1
+            // 
+            this.panel1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(244)))), ((int)(((byte)(0)))), ((int)(((byte)(0)))));
+            this.panel1.Controls.Add(this.btnExit);
+            this.panel1.Controls.Add(this.lblCustomerModule);
+            this.panel1.Dock = System.Windows.Forms.DockStyle.Top;
+            this.panel1.Location = new System.Drawing.Point(0, 0);
+            this.panel1.Margin = new System.Windows.Forms.Padding(4);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(607, 54);
+            this.panel1.TabIndex = 26;
+            // 
+            // CategoryModuleForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 17F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(607, 198);
+            this.Controls.Add(this.btnClearCategory);
+            this.Controls.Add(this.btnUpdateCategory);
+            this.Controls.Add(this.btnSaveCategory);
+            this.Controls.Add(this.txtCategoryName);
+            this.Controls.Add(this.lblCategoryName);
+            this.Controls.Add(this.lblCategoryID);
+            this.Controls.Add(this.panel1);
+            this.Font = new System.Drawing.Font("Century Gothic", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.Name = "CategoryModuleForm";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "CategoryModuleForm";
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        public System.Windows.Forms.Button btnClearCategory;
+        public System.Windows.Forms.Button btnUpdateCategory;
+        public System.Windows.Forms.Button btnSaveCategory;
+        public System.Windows.Forms.TextBox txtCategoryName;
+        private System.Windows.Forms.Label lblCategoryName;
+        private System.Windows.Forms.Button btnExit;
+        private System.Windows.Forms.Label lblCustomerModule;
+        public System.Windows.Forms.Label lblCategoryID;
+        private System.Windows.Forms.Panel panel1;
+    }
+}
