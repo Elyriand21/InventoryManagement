5e0e07f3b3cb282f01fdbec542f0e2cfc17f69fc
Felix <felix.banks02@gmail.com>
2024-09-20 14:25:10 -0400

    Fixed typos within the comments - now accurately reflects Customer in CustomerForm.cs rather than User

--- InventoryManagement/CustomerForm.cs
+++ InventoryManagement/CustomerForm.cs
@@ -29,7 +29,7 @@ namespace InventoryManagement
         {
             int i = 0;  // Creates a variable to track the rows
             dgvCustomers.Rows.Clear();  // Clears all of the rows in the grid so no confusion
-            cm = new SqlCommand("SELECT * FROM tbCustomer", con);   // Creates a new command telling the database to select ALL users from the users table
+            cm = new SqlCommand("SELECT * FROM tbCustomer", con);   // Creates a new command telling the database to select ALL customers from the customer table
             con.Open(); // Initiates the request to the database
             dr = cm.ExecuteReader();    // We use ExecuteReader since we will be receiving multiple entries from the database
 
@@ -37,13 +37,13 @@ namespace InventoryManagement
             while (dr.Read())
             {
                 i++;    // Increase the row number
-                dgvCustomers.Rows.Add(i, dr[0].ToString(), dr[1].ToString(), dr[2].ToString());   // Add the user that corresponds with the row
+                dgvCustomers.Rows.Add(i, dr[0].ToString(), dr[1].ToString(), dr[2].ToString());   // Add the customer that corresponds with the row
             }
             dr.Close(); // Since we're finished reading the information, close the reader
             con.Close();    // Close the connection to the SQL server
         }
 
-        // Method for adding a new user
+        // Method for adding a new customer
         private void btnCustomerAdd_Click(object sender, EventArgs e)
         {
             CustomerModuleForm customerModule = new CustomerModuleForm();   // Creates a new CustomerModule form
@@ -53,6 +53,7 @@ namespace InventoryManagement
             LoadCustomer(); // Loads the customer(s) from the database
         }
 
+        // Method that deals with what happens when the DataGrid is clicked on
         private void dgvCustomers_CellContentClick(object sender, DataGridViewCellEventArgs e)
         {
             string colName = dgvCustomers.Columns[e.ColumnIndex].Name;  // Whenever a cell is clicked, this indicates the name of the column
@@ -60,24 +61,24 @@ namespace InventoryManagement
             // If the column of the cell that's selected is EDIT
             if (colName == "Edit")
             {
-                // Create a new UserModuleForm
-                CustomerModuleForm customerModule = new CustomerModuleForm();
-                customerModule.lblCustomerID.Text = dgvCustomers.Rows[e.RowIndex].Cells[1].Value.ToString();
-                customerModule.txtCustomerName.Text = dgvCustomers.Rows[e.RowIndex].Cells[2].Value.ToString();      //
-                customerModule.txtCustomerPhone.Text = dgvCustomers.Rows[e.RowIndex].Cells[3].Value.ToString();         //
+                CustomerModuleForm customerModule = new CustomerModuleForm();   // Create a new CustomerModuleForm
+                customerModule.lblCustomerID.Text = dgvCustomers.Rows[e.RowIndex].Cells[1].Value.ToString();        //
+                customerModule.txtCustomerName.Text = dgvCustomers.Rows[e.RowIndex].Cells[2].Value.ToString();      //  Fills the appropriate information from the
+                customerModule.txtCustomerPhone.Text = dgvCustomers.Rows[e.RowIndex].Cells[3].Value.ToString();     //  datagrid
 
-                customerModule.btnSaveCustomer.Enabled = false; // Disable the save button since we're not adding new users
-                customerModule.btnUpdateCustomer.Enabled = true;    // Since we're editing the user, we need to update the user
+                customerModule.btnSaveCustomer.Enabled = false; // Disable the save button since we're not adding new customers
+                customerModule.btnUpdateCustomer.Enabled = true;    // Since we're editing the customer, we need to update the customer
                 customerModule.ShowDialog();    // Show the CustomerModuleForm
             }
             // If the column is DELETE
             else if (colName == "Delete")
             {
-                // Display a box assuring that user wants to delete the selected user
+                // Display a box assuring that user wants to delete the selected customer
                 if (MessageBox.Show("Are you sure you want to delete this customer?", "Delete Record", MessageBoxButtons.YesNo, MessageBoxIcon.Question) == DialogResult.Yes)
                 {
                     con.Open(); // Opens the connection to the SQL server
-                    // Creates a SQL commands to delete the entry in the database that matches the username
+
+                    // Creates a SQL commands to delete the entry in the database that matches the customerId
                     cm = new SqlCommand("DELETE FROM tbCustomer WHERE customerId LIKE '" + dgvCustomers.Rows[e.RowIndex].Cells[1].Value.ToString() + "'", con);
                     cm.ExecuteNonQuery();   // Tells the database to insert. We use this instead of ExecuteQuery because we're modifying, not querying
                     con.Close();    // Closes the connection to the SQL server
